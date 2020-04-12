6b1b610c8894affc82295f02759dfa668905debb
CordellStocker <cstocker@karmak.com>
2020-04-12 16:17:18 -0500

    json as stringssssssss

--- src/main/java/zoot/tube/webserver/YouTubeAPIWebAdapter.java
+++ src/main/java/zoot/tube/webserver/YouTubeAPIWebAdapter.java
@@ -1,6 +1,10 @@
 package zoot.tube.webserver;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.List;
 import zoot.tube.googleapi.YouTubeAPI;
+import zoot.tube.googleapi.ZTPlaylist;
 
 public class YouTubeAPIWebAdapter {
 
@@ -10,7 +14,19 @@ public class YouTubeAPIWebAdapter {
         this.youtubeAPI = youtubeAPI;
     }
 
-    public void getMyPlaylistsAsJSON() {
+    public String getMyPlaylistsAsJsonString() {
+        List<ZTPlaylist> playlists = this.youtubeAPI.getMyPlaylists();
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("{\"data\":[");
+        for (int i = 0; i < playlists.size() - 1; i++) {
+            sb.append(playlists.get(i).toJsonString()).append(",");
+        }
+        if (playlists.size() > 0) {
+            sb.append(playlists.get(playlists.size() - 1).toJsonString());
+        }
+        sb.append("]}");
+
+        return sb.toString();
     }
 }
